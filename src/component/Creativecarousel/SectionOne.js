7e69158d8e975ba8103b8fa0d7ab41561281bdba
Munish Verma <verma.munish16@gmail.com>
2021-10-07 00:01:00 -0400

    revert back the title

--- src/component/Creativecarousel/SectionOne.js
+++ src/component/Creativecarousel/SectionOne.js
@@ -17,7 +17,7 @@ class SectionOne extends Component {
                   </h6>
                   <h2>
                     <SplitText charPoses={charPoses}>
-                      DAPIP - NEW
+                      DAPIP
                     </SplitText>
                   </h2>
                   <p>
